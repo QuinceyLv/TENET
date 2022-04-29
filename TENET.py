b53bc3fccab36920943fb00f5e5a4aeec83d30ab
QuinceyLyu <quincey@bemani.cn>
2022-04-29 10:35:34 +0800

    main

--- TENET.py
+++ TENET.py
@@ -181,8 +181,8 @@ def main(branch, trajectory1SortIndex, cell_gene_all, gene_name, out):
 
 if __name__ == "__main__":
     if args.filelist:
-        process_file_list(expr ,traj, cell)
+        branch, trajectory1SortIndex, cell_gene_all, gene_name = process_file_list(expr ,traj, cell)
     else:
-        process_h5ad(h5ad)
+        branch, trajectory1SortIndex, cell_gene_all, gene_name = process_h5ad(h5ad)
 
-    main()
+    main(branch, trajectory1SortIndex, cell_gene_all, gene_name)
